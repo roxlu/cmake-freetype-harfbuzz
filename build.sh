8c7b10f5a36a22f7c1fa80d4c7026c7ad8382523
roxlu <diederick@roxlu.com>
2020-03-01 14:21:23 +0100

    Added Linux files.

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+d=${PWD}
+bd=${d}/build
+id=${d}/installed
+
+if [ ! -d ${bd} ] ; then
+    mkdir ${bd}
+fi
+
+cd ${bd}
+cmake -G "Ninja" \
+      -DCMAKE_INSTALL_PREFIX=${id} \
+      -DCMAKE_BUILD_TYPE="Release" \
+      ..
+
+if [ $? -ne 0 ] ; then
+    echo "Failed to configure"
+    exit
+fi
+
+# ninja
+cmake --build . --target install
+
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+
+  printf("\nFreeType with Harfbuzz Test\n");
+
+  return 0;
+}
